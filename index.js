ee481d5dbb9e73bb656b77dfc5c72efd10d28b1f
Quicksilver_YT <96615931+QuicksilverYT@users.noreply.github.com>
2022-05-05 14:06:16 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const { Client, Message, MessageEmbed, Collection } = require("discord.js");
+const db =require("quick.db");
+const fs = require("fs");
+const config = require("./config.json");
+const dotenv = require("dotenv").config();
+const token = process.env.TOKEN;
+
+const client = new Client({
+  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  restWsBridgetimeout: 100,
+  shards: "auto",
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: 32767,
+});
+module.exports = client;
+
+require("./events/message.js")
+require("./events/ready.js")
+
+client.commands = new Collection();
+client.aliases = new Collection();
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.error(err);
+  console.log(`${files.length} Total Command!`);
+  files.forEach(f => {
+    let props = require(`./commands/${f}`);
+    console.log(`${props.help.name} command is loaded!`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+// Do not change anything here
+require('http').createServer((req, res) => res.end(`
+ |-----------------------------------------|
+ |              Informations               |
+ |-----------------------------------------|
+ |• Alive: 24/7                            |
+ |-----------------------------------------|
+ |• Author: itzQuicksilver#8750            |
+ |-----------------------------------------|
+ |• Server: https://discord.gg/TaynAW9WXt  |
+ |-----------------------------------------|
+ |• Github https://github.com/quicksilveryt|
+ |-----------------------------------------|
+ |• License: Apache License 2.0            |
+ |-----------------------------------------|
+`)).listen(3000) //Dont remove this 
+
+client.login(token); 
